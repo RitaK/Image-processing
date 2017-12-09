41a1ab116d0c42b54f45328a648aac3d6cad3848
Rita Kaufman <rita.kaufman@sap.com>
2017-12-09 17:32:09 +0200

    ex7 added

--- Chapter1/Ex7.py
+++ Chapter1/Ex7.py
@@ -1,29 +1,41 @@
 import numpy as np
 import cv2
 
-Iin1 = cv2.imread('lulu1.jpg', cv2.IMREAD_GRAYSCALE)
-cv2.imshow('cameraman', Iin1)
+Iin1 = cv2.imread('kidface.jpg', cv2.IMREAD_GRAYSCALE)
+cv2.imshow('kidface', Iin1)
 
 
 d1,d2 = Iin1.shape
 print(d1,d2)
 
-Iout1 = Iin1[::2,::2]
-print Iout1.size
 
-cv2.imshow("output1", Iout1)
+Iout2 = np.zeros([d1*2, d2*2], np.uint8)
 
-Iout2 = np.zeros([d1, d2], np.uint8)
-
-for i in range(d1/2):
-    for j in range(d2/2):
+for i in range(d1):
+    for j in range(d2):
         k = 2*i
         m = 2*j
-        num = Iout1[i, j]
-        Iout2[k, m] = num
-        Iout2[k, m+1] = num
-        Iout2[k+1, m] = num
-        Iout2[k + 1, m+1] = num
+        num = Iin1[i, j]
+        if 51 <=num <= 101:
+            Iout2[k + 1, m] = 255
+        else:
+            if 102 <= num <= 152:
+                Iout2[k+1, m] =255
+                Iout2[k, m+1] = 255
+            else:
+                if 153 <= num <= 203:
+                    Iout2[k+1, m+1] = 255
+                    Iout2[k, m+1] = 255
+                    Iout2[k+1, m] = 255
+                else:
+                    if 204 <= num <= 255:
+                        Iout2[k, m] = 255
+                        Iout2[k+1, m+1] = 255
+                        Iout2[k+1, m] = 255
+                        Iout2[k, m+1] = 255
+
+
+
 
 # print Iout2.size
 
